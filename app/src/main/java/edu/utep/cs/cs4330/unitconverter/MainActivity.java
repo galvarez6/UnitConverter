5cfbb9fc95d37b9cbd979be4f8260220317e431f
Gilbert Alvarez <galvarez6@miners.utep.edu>
2020-10-06 09:53:03 -0600

    first commit

---
+++ app/src/main/java/edu/utep/cs/cs4330/unitconverter/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.utep.cs.cs4330.unitconverter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    UnitConverter uni = new UnitConverter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        uni.setMode(UnitConverter.Mode.POUND_TO_GRAM);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.f_m) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
